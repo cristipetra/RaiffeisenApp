cc3064375020d70e2e69a064555254e0b965c2fd
Cristian Petra <cpetra@qlaunch.de>
2026-07-23 17:18:00 +0300

    Initial Commit

---
+++ RaiffeisenApp/RaiffeisenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiffeisenAppApp.swift
+//  RaiffeisenApp
+//
+//  Created by Cristian Petra on 23.07.26.
+//
+
+import SwiftUI
+
+@main
+struct RaiffeisenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaiffeisenAppTests/RaiffeisenAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RaiffeisenAppTests.swift
+//  RaiffeisenAppTests
+//
+//  Created by Cristian Petra on 23.07.26.
+//
+
+import Testing
+@testable import RaiffeisenApp
+
+struct RaiffeisenAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
